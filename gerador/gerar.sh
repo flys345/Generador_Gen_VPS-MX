8f0e892c86de0c537966bd6bc51f02fcec217720
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-03 01:11:27 -0300

    Update gerar.sh

--- gerador/gerar.sh
+++ gerador/gerar.sh
@@ -74,7 +74,7 @@ echo $system|awk '{print $1, $2}'
 meu_ip () {
 MEU_IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 MEU_IP2=$(wget -qO- ipv4.icanhazip.com)
-[[ "$MEU_IP" != "$MEU_IP2" ]] && echo "$MEU_IP2" || echo "$MEU_IP"
+[[ "$MEU_IP" != "$MEU_IP2" ]] && "$MEU_IP2" || "$MEU_IP"
 }
 
 # EXECUCION DE MENU
