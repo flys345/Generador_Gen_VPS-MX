9c9cd2fc8d94b0e05522b5496388ebc8b4a5d22d
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-03 11:48:55 -0300

    Update gerar.sh

--- gerador/gerar.sh
+++ gerador/gerar.sh
@@ -98,11 +98,10 @@ echo -e "\e[97m\033[1;41m       =====>>►► 🐲 GEN VPS•MX 🐲 ◄◄<<===
 msg -bar
 msg -ne "   S.O: " && echo -ne "\033[1;37m$(os_system)"
 msg -ne "         IP: " && echo -e "\033[1;37m$(meu_ipe)"
-echo -e "   \033[1;31mRAM                         CPU"
-echo -e "   \033[1;31mTOTAL: \033[1;32m$ram1             \033[1;31mNUCLEOS: \033[1;32m $_core"
-echo -e "   \033[1;31mUSADA: \033[1;32m$ram3             \033[1;31mUSO DE CPU: \033[1;32m$_usop"
+echo -e "   \033[1;31mRAM: \033[1;32m$ram1                   \033[1;31mCPU: \033[1;32m $_core"
+echo -e "   \033[1;31mUSADA: \033[1;32m$ram3                 \033[1;31mUSO DE CPU: \033[1;32m$_usop"
 echo -e "   \033[1;31mLIBRE: \033[1;32m$ram2"
-echo -e "   \033[1;31mUSO DE RAM: \033[1;32m$_usor"
+echo -e "   \033[1;31mUSO DE RAM: \033[1;32m$_usor           \033[1;31mFECHA: \033[1;37m$_hora"
 
 
 #msg -ne "   FECHA: " && echo -ne "\033[1;37m$_hora"
