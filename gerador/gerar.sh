a6aae40ad6c71f07cfdf24f9719ee0d1e6bfc9b8
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-04 03:18:01 -0300

    Update gerar.sh

--- gerador/gerar.sh
+++ gerador/gerar.sh
@@ -121,9 +121,9 @@ MIP2=$(wget -qO- ipv4.icanhazip.com)
 }
 
 mudar_instacao () {
+clear
 while [[ ${var[$value]} != 0 ]]; do
 [[ -e /etc/newadm-instalacao ]] && BASICINST="$(cat /etc/newadm-instalacao)" || BASICINST="ADMbot.sh C-SSR.sh Crear-Demo.sh PDirect.py PGet.py POpen.py PPriv.py PPub.py Shadowsocks-R.sh Shadowsocks-libev.sh Unlock-Pass-VULTR.sh apacheon.sh blockBT.sh budp.sh dns-netflix.sh   dropbear.sh fai2ban.sh gestor.sh menu message.txt openvpn.sh paysnd.sh ports.sh shadowsocks.sh sockspy.sh speed.sh speedtest.py squid.sh squidpass.sh ssl.sh tcp.sh ultrahost usercodes utils.sh v2ray.sh"
-clear
 msg -bar
 echo -e "MENÚ SELECCIÓN DE INSTALACIÓN"
 msg -bar
