dc4f7fd098e25d7178adcade0fedd4c8be96253c
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-07 22:11:00 -0300

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+BARRA="\033[1;36m-----------------------------------------------------\033[0m"
 IVAR="/etc/http-instas"
 SCPT_DIR="/etc/SCRIPT"
+SCPresq="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3J1ZGk5OTk5L0dlbmVyYWRvcl9HZW5fVlBTLU1YL21hc3Rlci9nZXJhZG9y"
+SUB_DOM='base64 -d'
 rm $(pwd)/$0
 
 ofus () {
@@ -39,44 +42,52 @@ esac
 mv -f $HOME/$1 ${ARQ}/$1
 chmod +x ${ARQ}/$1
 }
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
-echo -e "\033[1;36m--------------------KEY GENERATOR BY MARCHNICK----------------------\033[0m"
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
-read -p "INTRODUZCA SU KEY DE INSTALACIÓN: " Key
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
-[[ ! $Key ]] && {
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
-echo -e "\033[1;33mKey inválida!"
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
-exit
-}
 meu_ip () {
 MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 MIP2=$(wget -qO- ipv4.icanhazip.com)
 [[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
 echo "$IP" > /usr/bin/vendor_code
 }
+function_verify () {
+  permited=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/Generador_Gen_VPS-MX/master/Control-IP")
+  [[ $(echo $permited|grep "${IP}") = "" ]] && {
+  echo -e "\n\n\n\033[1;31m====================================================="
+  echo -e "\033[1;31m       ¡LA IP $(wget -qO- ipv4.icanhazip.com) NO ESTA AUTORIZADA!"
+  echo -e "\033[1;31m                CONTACTE A @Rufu99"
+  echo -e "\033[1;31m=====================================================\n\n\n"
+  [[ -d /etc/SCRIPT ]] && rm -rf /etc/SCRIPT
+  exit 1
+  } || {
+  ### INTALAR VERCION DE SCRIPT
+  v1=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/Generador_Gen_VPS-MX/master/Vercion")
+  echo "$v1" > /etc/versin_script
+  }
+}
 meu_ip
-echo -e "\033[1;33mVerificando key... "
+function_verify
+echo -e "$BARRA"
+echo -e "\033[1;33mDescargando archivos... "
+echo -e "$BARRA"
 cd $HOME
-wget -O "$HOME/lista-arq" $(ofus "$Key")/$IP > /dev/null 2>&1
-IP=$(ofus "$Key" | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+REQUEST=$(echo $SCPresq|$SUB_DOM)
+wget -O "$HOME/lista-arq" ${REQUEST}/GERADOR > /dev/null 2>&1
 sleep 1s
 [[ -e $HOME/lista-arq ]] && {
-REQUEST=$(ofus "$Key" |cut -d'/' -f2)
 for arqx in `cat $HOME/lista-arq`; do
-echo -ne "\033[1;33mDescargando \033[1;31m[$arqx] "
-wget -O $HOME/$arqx ${IP}:81/${REQUEST}/${arqx} > /dev/null 2>&1 && echo -e "\033[1;31m- \033[1;32mRecibido!" || echo -e "\033[1;31m- \033[1;31mFalla (no recibido!)"
+echo -ne "\033[1;33mDescargando: \033[1;31m[$arqx] "
+wget -O $HOME/$arqx ${REQUEST}/${arqx} > /dev/null 2>&1 && {
+echo -e "\033[1;31m- \033[1;32mRecibido!"
 [[ -e $HOME/$arqx ]] && veryfy_fun $arqx
+} || echo -e "\033[1;31m- \033[1;31mFalla (no recibido!)"
 done
-[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://www.dropbox.com/s/l6iqf5xjtjmpdx5/trans?dl=0 &> /dev/null
-mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
-apt-get install bc -y &>/dev/null
-apt-get install screen -y &>/dev/null
-apt-get install nano -y &>/dev/null
-apt-get install curl -y &>/dev/null
-apt-get install netcat -y &>/dev/null
-apt-get install apache2 -y &>/dev/null
+[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://raw.githubusercontent.com/rudi9999/Generador_Gen_VPS-MX/master/Install/trans &> /dev/null
+[[ -e /bin/http-server.py ]] && mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
+[[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || apt-get install bc -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "screen"|head -1) ]] || apt-get install screen -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "nano"|head -1) ]] || apt-get install nano -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "curl"|head -1) ]] || apt-get install curl -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "netcat"|head -1) ]] || apt-get install netcat -y &>/dev/null
+[[ $(dpkg --get-selections|grep -w "apache2"|head -1) ]] || apt-get install apache2 -y &>/dev/null
 sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
 service apache2 restart > /dev/null 2>&1 &
 IVAR2="/etc/key-gerador"
@@ -90,12 +101,17 @@ cd /etc/SCRIPT
 rm -rf FERRAMENTA KEY KEY! INVALIDA!
 rm $HOME/lista-arq
 sed -i -e 's/\r$//' /usr/bin/gerar.sh
+echo -e "$BARRA"
+echo "/usr/bin/gerar.sh" > /usr/bin/gerar && chmod +x /usr/bin/gerar
 echo -e "\033[1;33m Perfecto, utilize el comando \033[1;31mgerar.sh o gerar \033[1;33mpara administrar sus keys y
  actualizar la base del servidor"
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "$BARRA"
 } || {
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
-echo -e "\033[1;33mKey inválida!"
-echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
+echo -e "$BARRA"
+echo -e "\033[1;33mKey Invalida!"
+echo -e "$BARRA"
 }
-echo -ne "\033[0m"1
+echo -ne "\033[0m"
+apt-get install netcat -y &>/dev/null
+apt-get install net-tools -y &>/dev/null
+sed -i -e 's/\r$//' /usr/bin/gerar.sh
